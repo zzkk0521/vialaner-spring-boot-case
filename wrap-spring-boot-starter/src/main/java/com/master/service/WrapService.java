7fe5fb0bf453184b98acefcc91c654288f7e2e39
master0521 <zzkk0521@163.com>
2020-06-29 00:10:54 +0800

    zzkk0521/zk-spring-boot-starter

--- wrap-spring-boot-starter/src/main/java/com/master/service/WrapService.java
+++ wrap-spring-boot-starter/src/main/java/com/master/service/WrapService.java
@@ -12,7 +12,7 @@ public class WrapService {
     private String after;
 
     public String wrap(String param){
-        System.out.println("vialaner_199206210606");
+        System.out.println("vialaner_0521");
         return before + param + after;
     }
 
